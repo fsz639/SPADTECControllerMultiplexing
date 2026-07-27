7b0d62179132aaf6dd46ecb474ee91b1a8241f72
fsz639 <ferran639@gmail.com>
2026-07-27 16:13:35 +0000

    Cleaning project

--- BBBSqSignals/BashMakeFileBBBsignalQuadraturecpp.sh
+++
@@ -1,1 +0,0 @@
-arm-linux-gnueabihf-g++ -static -I ../am335x_pru_package/pru_sw/app_loader/include BBBsignalQuadrature.cpp ../am335x_pru_package/pru_sw/app_loader/interface/prussdrv.o -o BBBsignalQuadrature
